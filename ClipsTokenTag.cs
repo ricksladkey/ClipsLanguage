fb214111d610bb1803313c3a70173c90842ad3f0
Rick Sladkey <rick.sladkey@gmail.com>
2013-11-23 03:17:10 -0500

    Add CLIPS lexical tokenizer.

--- ClipsTokenTag.cs
+++ ClipsTokenTag.cs
@@ -12,6 +12,7 @@ namespace ClipsLanguage
     using Microsoft.VisualStudio.Text.Tagging;
     using Microsoft.VisualStudio.Utilities;
     using System.Diagnostics;
+    using System.Text;
 
     [Export(typeof(ITaggerProvider))]
     [ContentType("Clips")]
@@ -56,6 +57,101 @@ namespace ClipsLanguage
             remove { }
         }
 
+        internal IEnumerable<string> Tokenize(string text)
+        {
+            int position = 0;
+            int length = text.Length;
+            while (position < length)
+            {
+                var c = text[position];
+                var buffer = new StringBuilder();
+
+                if ("()&|~".IndexOf(c) != -1)
+                {
+                    yield return c.ToString();
+                    ++position;
+                    continue;
+                }
+
+                if (c == '"')
+                {
+                    ++position;
+                    while (position < length)
+                    {
+                        c = text[position];
+                        if (c == '"')
+                            break;
+                        if (c == '\\')
+                        {
+                            buffer.Append(c);
+                            ++position;
+                            if (position < length)
+                            {
+                                c = text[position];
+                                buffer.Append(c);
+                                ++position;
+                            }
+                        }
+                        else
+                        {
+                            buffer.Append(c);
+                            ++position;
+                        }
+                    }
+                    yield return buffer.ToString();
+                    continue;
+                }
+
+                if (char.IsWhiteSpace(c))
+                {
+                    buffer.Append(c);
+                    ++position;
+                    while (position < length)
+                    {
+                        c = text[position];
+                        if (!char.IsWhiteSpace(c))
+                            break;
+                        buffer.Append(c);
+                        ++position;
+                    }
+                    yield return buffer.ToString();
+                    continue;
+
+                }
+
+                if (c == ';')
+                {
+                    buffer.Append(c);
+                    ++position;
+                    while (position < length)
+                    {
+                        c = text[position];
+                        buffer.Append(c);
+                        ++position;
+                    }
+                    yield return buffer.ToString();
+                    continue;
+
+                }
+
+                if (!char.IsWhiteSpace(c))
+                {
+                    buffer.Append(c);
+                    ++position;
+                    while (position < length)
+                    {
+                        c = text[position];
+                        if (char.IsWhiteSpace(c) || "()&|<~".IndexOf(c) != -1)
+                            break;
+                        buffer.Append(c);
+                        ++position;
+                    }
+                    yield return buffer.ToString();
+                    continue;
+                }
+            }
+        }
+
         public IEnumerable<ITagSpan<ClipsTokenTag>> GetTags(NormalizedSnapshotSpanCollection spans)
         {
             //Debug.WriteLine("GetTags");
@@ -63,34 +159,32 @@ namespace ClipsLanguage
             {
                 //Debug.WriteLine("curSpan = {0}", curSpan);
                 ITextSnapshotLine containingLine = curSpan.Start.GetContainingLine();
-                int lineStart = containingLine.Start.Position;
-                int position = lineStart;
-                string line = containingLine.GetText();
-                int commentStart = line.IndexOf(';');
-                string code = line;
+                var lineStart = containingLine.Start.Position;
+                var position = lineStart;
+                var line = containingLine.GetText();
+                var commentStart = line.IndexOf(';');
+                var code = line;
                 if (commentStart != -1)
                     code = code.Substring(0, commentStart);
-                string[] tokens = code.Split(' ');
+                var tokens = Tokenize(code);
 
-                foreach (string ClipsToken in tokens)
+                foreach (var token in Tokenize(code))
                 {
-                    if (_ClipsTypes.ContainsKey(ClipsToken))
+                    if (_ClipsTypes.ContainsKey(token))
                     {
                         var tokenSpan = new SnapshotSpan(curSpan.Snapshot,
-                            new Span(position, ClipsToken.Length));
+                            new Span(position, token.Length));
                         if (tokenSpan.IntersectsWith(curSpan)) 
                             yield return new TagSpan<ClipsTokenTag>(tokenSpan, 
-                                new ClipsTokenTag(_ClipsTypes[ClipsToken]));
+                                new ClipsTokenTag(_ClipsTypes[token]));
                     }
-
-                    //add an extra char location because of the space
-                    position += ClipsToken.Length + 1;
+                    position += token.Length;
                 }
 
                 if (commentStart != -1)
                 {
-                    int start = lineStart + commentStart;
-                    int end = line.Length - commentStart;
+                    var start = lineStart + commentStart;
+                    var end = line.Length - commentStart;
                     var tokenSpan = new SnapshotSpan(curSpan.Snapshot,
                         new Span(start, end));
                     if (tokenSpan.IntersectsWith(curSpan)) 
